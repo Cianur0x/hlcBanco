fa0fbf9478bf81211dd2358621c240852102bd7e
Mireya <mmedmer810@g.educaand.es>
2024-01-11 09:59:50 +0100

    commit2

--- src/main/java/org/iesvdm/Main.java
+++ src/main/java/org/iesvdm/Main.java
@@ -1,12 +1,17 @@
 package org.iesvdm;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
+    private static final String ARCHIVO_MOVIMIENTOS = "movimientos.txt";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double saldo = 1000.0;
@@ -51,10 +56,25 @@ public class Main {
 
 
     private static void registrarMovimiento(String tipo, double cantidad) {
-        // todo
+        String mensaje = tipo + ": " + cantidad;
+        try {
+            Files.writeString(Path.of(ARCHIVO_MOVIMIENTOS), mensaje + System.lineSeparator(), StandardOpenOption.APPEND);
+            System.out.println("Movimiento registrado: " + mensaje);
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo de movimientos.");
+        }
     }
 
     private static void consultarMovimientos() {
-        // todo
+        try {
+            List<String> lineas = Files.readAllLines(Path.of(ARCHIVO_MOVIMIENTOS));
+            System.out.println("Historial de movimientos:");
+            for (String linea : lineas) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo de movimientos.");
+        }
     }
+
 }
